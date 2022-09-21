9639300780002020e6640024ac78a354f6bd6b82
cathe <cacateles12@gmail.com>
2022-09-21 19:16:00 -0300

    Added user controller to create new user

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 require('express-async-errors');
 const errorHandler = require('../middlewares/errohandler');
 const login = require('../routes/login');
+const user = require('../routes/user');
 
 const app = express();
 app.use(express.json());
 app.use('/login', login);
+app.use('/user', user);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -1,7 +1,7 @@
 const userService = require('../services/usersService');
 
 const userController = {
-  login: async (req, res) => {
+  create: async (req, res) => {
     await userService.validateBody(req.body);
     const result = await userService.create(req.body);
 
