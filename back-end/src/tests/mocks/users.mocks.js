b1818bee07270d2bfd4186337926525429dde222
cathe <cacateles12@gmail.com>
2022-09-22 19:30:15 -0300

    Starting saleService

---
+++ back-end/src/tests/mocks/users.mocks.js
@@ -0,0 +1,11 @@
+const userValid = {
+  "email": "adm@deliveryapp.com",
+  "password": "--adm2@21!!--",
+  "name": "Delivery App Admin"
+};
+
+const userNotValid = {
+  "email": "admdeliveryapp.com",
+  "password": "--adm2@21!!--",
+  "name": "Delivery App Admin"
+};
