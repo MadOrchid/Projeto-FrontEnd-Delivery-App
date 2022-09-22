8a2f5afcc6f90772defc8e72759b12fe39029435
TH.ThiagoAvels <thiagoavels1@gmail.com>
2022-09-22 18:07:13 -0300

    feat: teste de requisitos

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('express-async-errors');
-const errorHandler = require('../middlewares/errohandler');
 const cors = require('cors');
+const errorHandler = require('../middlewares/errohandler');
 const login = require('../routes/login');
 const user = require('../routes/user');
 
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.use('/customer/products', login);
+app.use('/login', login);
 app.use('/user', user);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
