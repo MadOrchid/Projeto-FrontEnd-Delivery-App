05487735eecdf145f988a2914246dba53328bbf9
TH.ThiagoAvels <thiagoavels1@gmail.com>
2022-09-23 00:47:26 -0300

    Cards Prontos

--- front-end/src/components/CardsProducts/index.js
+++ front-end/src/components/CardsProducts/index.js
@@ -1,45 +1,57 @@
 import React from 'react';
 
+/*
+  - 15: customer_products__element-card-title-<id>
+  - 16: customer_products__element-card-price-<id>
+  - 17: customer_products__img-card-bg-image-<id>
+  - 18: customer_products__button-card-add-item-<id>
+  - 19: customer_products__button-card-rm-item-<id>
+  - 20: customer_products__input-card-quantity-<id>
+  - 21: customer_products__checkout-bottom-value
+*/
+
+/* async function handleCards() {
+  await api.get('products')
+    .catch((e) => {
+      throw e.message;
+    });
+} */
+
 function CardsProducts() {
   return (
     <section>
       <h1>CardsProducts</h1>
       {
-      /*
-        data.formap(element => {
-
-        });
-
-      - 15: customer_products__element-card-title-<id>
-      - 16: customer_products__element-card-price-<id>
-      - 17: customer_products__img-card-bg-image-<id>
-      - 18: customer_products__button-card-add-item-<id>
-      - 19: customer_products__button-card-rm-item-<id>
-      - 20: customer_products__input-card-quantity-<id>
-      - 21: customer_products__checkout-bottom-value
-      */
+        /*
+        handleCards().map((item) => (
+          <>
+            <h3
+              data-testid={ `customer_products__element-card-title-${item.id}` }
+            >
+              {item.price}
+            </h3>
+            <img
+              src={ item.url_image }
+              alt="imagem da bebida"
+              data-testid={ `customer_products__img-card-bg-image-${item.id}` }
+            />
+            <h4 data-testid="customer_products__element-card-title-">{item.name}</h4>
+            <input
+              type="button"
+              value="+"
+              alt="Adicionar produto"
+              data-testid={ `customer_products__button-card-add-item-${item.id}` }
+            />
+            <p data-testid={ `customer_products__input-card-quantity-${item.id}` }>0</p>
+            <input
+              type="button"
+              value="-"
+              alt="Adicionar produto"
+              data-testid={ `customer_products__button-card-rm-item-${item.id}` }
+            />
+          </>
+        )) */
       }
-      <h3 data-testid="customer_products__element-card-title-">valor</h3>
-      <img
-        src="endereço"
-        alt="imagem da bebida"
-        data-testid="customer_products__img-card-bg-image-"
-      />
-      <h4 data-testid="customer_products__element-card-title-">nome</h4>
-      <input
-        type="button"
-        value="+"
-        alt="Adicionar produto"
-        data-testid="customer_products__button-card-add-item-"
-      />
-      <p data-testid="customer_products__input-card-quantity-">quantidade</p>
-      <input
-        type="button"
-        value="-"
-        alt="Adicionar produto"
-        data-testid="customer_products__button-card-rm-item-"
-      />
-
       <br />
       <br />
       <button
