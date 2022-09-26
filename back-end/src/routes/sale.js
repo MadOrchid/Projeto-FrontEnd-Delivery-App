ad31924fee2dfe47b3123a0cee9d01ed47b0eb8a
Rafael Moreci <rafamoreci@gmail.com>
2022-09-26 15:07:23 -0300

    feat: adicionado validação do token nas rotas product e sale

--- back-end/src/routes/sale.js
+++ back-end/src/routes/sale.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
+const authController = require('../controllers/authController');
 const saleController = require('../controllers/saleController');
 
 const sale = Router();
+
+sale.use(authController.validateToken);
 
 sale.post('/', saleController.create);
 
