f52875f72ca8aafc3a21dcfce6c3a2c51bdfe481
Rafael Moreci <rafamoreci@gmail.com>
2022-09-26 14:14:48 -0300

    feat: Remoção de código de testes da rota + início da implementação dos testes

--- back-end/src/services/salesProductsService.js
+++ back-end/src/services/salesProductsService.js
@@ -7,14 +7,7 @@ const salesProductsServices = {
       productId,
       quantity,
     }));
-    const test = addProducts[0];
-    const { productId, quantity } = test;
-    await SalesProducts.create({ saleId, productId, quantity });
-    // const saleProducts = await SalesProducts.bulkCreate([addProducts]);
-    // const result = await Promise.all(products.map(async (product) => {
-    //   const { productId, quantity } = product;
-    //   await SalesProducts.create({ saleId, productId, quantity });
-    // }));
+    await SalesProducts.bulkCreate(addProducts);
   },
 };
 
