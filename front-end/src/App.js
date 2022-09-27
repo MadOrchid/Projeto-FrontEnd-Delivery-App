826112f97753022cc18b128f6f371e277b307e05
Leonardo Begnossi <89605267+MadOrchid@users.noreply.github.com>
2022-09-27 19:48:35 -0300

    fix:error lint

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
 import Routes from './Routes';
-// import Provider from './context/Provider';
-import CartProvider from './context/ProviderGlobal';
 
 function App() {
   return (
--- front-end/src/components/CardsProducts/index.js
+++ front-end/src/components/CardsProducts/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import ContextGlobal from '../../context/ContextGlobal';
