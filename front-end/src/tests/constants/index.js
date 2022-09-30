2d2af17f4b4e5931c78520f0a4bb63e18a15cd71
Rafael Moreci <rafamoreci@gmail.com>
2022-09-30 16:59:17 -0300

    testes tela login

---
+++ front-end/src/tests/constants/index.js
@@ -0,0 +1,15 @@
+const userLoginEmailInputId = 'common_login__input-email';
+const userLoginPasswordInputId = 'common_login__input-password';
+const userLoginEnterButton = 'common_login__button-login';
+const userLoginRegisterButton = 'common_login__button-register';
+const userInputEmailMock = 'zebirita@email.com';
+const userInputPasswordMock = '$#zebirita#$';
+
+module.exports = {
+  userLoginEmailInputId,
+  userLoginPasswordInputId,
+  userLoginEnterButton,
+  userLoginRegisterButton,
+  userInputEmailMock,
+  userInputPasswordMock,
+};
